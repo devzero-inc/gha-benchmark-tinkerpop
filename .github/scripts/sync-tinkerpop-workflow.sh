44e6af936432c5ab8af4f0d8cf9517eacab0dd7b
Debosmit Ray <dray92@uw.edu>
2025-01-17 00:25:30 +0530

    Update sync-tinkerpop-workflow.sh

--- .github/scripts/sync-tinkerpop-workflow.sh
+++ .github/scripts/sync-tinkerpop-workflow.sh
@@ -15,7 +15,7 @@ yq -P -i '(.jobs.* | select(.strategy == null)).strategy.matrix.os = ["ubuntu-24
 yq -P -i '(.jobs.* | select(.strategy != null)).strategy.matrix.os = ["ubuntu-24.04", "devzero-ubuntu-24.04"]' .github/workflows/benchmark.yml
 yq -P -i '.jobs.*.runs-on = "${{ matrix.os }}"' .github/workflows/benchmark.yml
 yq -P -i '(.jobs.*.steps.[] | select(.uses == "actions/checkout@v4").with.repository) = "apache/tinkerpop"' .github/workflows/benchmark.yml
-yq -P -i '(.jobs.*.steps.[] | select(.uses == "actions/checkout@v4").with.ref) = "main"' .github/workflows/benchmark.yml
+yq -P -i '(.jobs.*.steps.[] | select(.uses == "actions/checkout@v4").with.ref) = "master"' .github/workflows/benchmark.yml
 
 # Clean up
 rm -rf temp-tinkerpop
